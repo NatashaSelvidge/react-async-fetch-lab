5f8a2699420548b530e102a7f2ad469a38ae0063
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-29 15:38:58 -0700

    commit tree 2a3f08cbaa180edf993d3bfeb38504b6ee316c24

--- src/App.js
+++ src/App.js
@@ -1,1 +1,40 @@
-// create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      astros: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = "http://api.open-notify.org/astros.json";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((json) => {
+        this.setState({
+          astros: json.people,
+        });
+      });
+  }
+
+  allAstros = () => {
+    return this.state.astros.map((astro) => (
+      <p>
+        `{astro.name} of the {astro.craft}`
+      </p>
+    ));
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>{this.allAstros()}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+export default App;
